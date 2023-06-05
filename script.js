dd53673f9b981ec80930776eafaaed5da7874f6b
Austin <moreno.austin12@gmail.com>
2023-06-05 16:58:00 +0800

    added a footer

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let decimalCount = 0;
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         if (button.classList.contains('number')) {
-            if (display.textContent[0] == 0) {
+            if (display.textContent === '0') {
                 display.textContent = '';
                 display.textContent += button.textContent
             } else {
